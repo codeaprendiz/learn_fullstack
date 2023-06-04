f0e4230ed113be6a16a93ebf8508952f3d391086
codeaprendiz <codeaprendiz@gmail.com>
2023-06-04 16:19:02 +0400

    chore: php auto-readme auto readme update

---
+++ gen-global-readme.php
@@ -0,0 +1,74 @@
+<?php
+
+$global_readme="";
+
+$languages = array(
+    "css",
+    "go",
+    "javascript",
+    "nodejs",
+    "reactjs",
+);
+
+// $languages = array(
+//     "### css" => "[css](css)",
+//     "### go" => "[go](go)",
+//     "### javascript" => "[javascript](javascript)",
+//     "### nodejs" => "[nodejs](nodejs)",
+//     "### reactjs" => "[reactjs](reactjs)",
+// );
+
+function getDirectories($directoryPath, $pattern) {
+    $directoryIterator = new RecursiveDirectoryIterator($directoryPath, RecursiveDirectoryIterator::SKIP_DOTS);
+    $recursiveIterator = new RecursiveIteratorIterator($directoryIterator, RecursiveIteratorIterator::CHILD_FIRST);
+
+    $directories = [];
+
+    foreach($recursiveIterator as $path) {
+        if ($path->isDir() && preg_match($pattern, $path->getFilename())) {
+            $directories[] = $path->getPathname();
+        }
+    }
+
+    // Sort the array
+    sort($directories);
+
+    return $directories;
+}
+
+// Function that create a global global_readme.md file
+function createGlobalglobal_readme($languages) {
+    $global_readme = "# Global global_readme\n\n";
+
+    foreach($languages as $language) {
+        $global_readme .= "- [" . $language . "](" . $language . ")" . "\n";
+    }
+
+    $taskDirectories = getDirectories('.', '/task-/');
+    foreach($languages as $language) {
+        $global_readme .= "\n## " . $language . "\n\n";
+        foreach($taskDirectories as $taskDirectory) {
+            // $taskDirectory is of form - [281] => ./reactjs/taskset/task-004-eslint-setup
+            // if $taskDirectory does not contain the language string, continue
+            if (strpos($taskDirectory, "./".$language) === false) {
+                continue;
+            }
+
+            // save only the task-004-eslint-setup part of the path in a new variable without changing the $taskDirectory by getting the part after last /
+            
+            $taskName = substr($taskDirectory, strrpos($taskDirectory, '/') + 1);
+            $global_readme .= "- [" . $taskName . "](" . $taskDirectory . "/"  . ")\n";
+
+            // $taskName = str_replace('./', '', $taskDirectory);
+            // $global_readme .= "- [" . $taskName . "](" . $taskName . "/"  . ")\n";
+        }
+    }
+
+    file_put_contents('Readme.md', $global_readme);
+}
+
+$batchDirectories = getDirectories('.', '/task-/');
+// echo '<pre>'; print_r($batchDirectories); echo '</pre>';
+createGlobalglobal_readme($languages);
+
+?>
