1626d3b85aa92b033efe92ff918b3462a17fc8dd
codeaprendiz <codeaprendiz@gmail.com>
2023-06-04 16:49:17 +0400

    chore: sync all logic

--- sync-all.sh
+++ sync-all.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Exit script on first error
+set -e
+
 # Sync nodejs progress
-bash ./nodejs/sync-nodejs.sh
+bash ./nodejs/rsync-nodejs.sh
+
+# Sync go progress
+bash ./go/rsync-go.sh
